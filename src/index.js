e9d24be4891e21c9582bc04ecae4bd71957d7618
preksha3005 <preksha.g504@gmail.com>
2024-05-23 21:02:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function Pass() {
+  const [pass, setp] = React.useState("");
+  const [msg, setm] = React.useState("");
+  const [colour,setc]=React.useState("")
+  function getscore(pass) {
+    let score=0;
+    if (pass.length > 8) score += 1;
+    if (/[a-z]/.test(pass)) score += 1;
+    if (/[A-Z]/.test(pass)) score += 1;
+    if (/\d/.test(pass)) score += 1;
+    if (/[^A-Za-z0-9]/.test(pass)) score += 1;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    switch (score) {
+      case 0:
+      case 1:
+      case 2:
+        setc("red")
+        return "Weak";
+        // break;
+      case 3:
+        setc("orange")
+        return "Medium";
+        // break;
+      case 4:
+      case 5:
+        setc("green")
+        return "Strong";
+        // break;
+      default:
+        return "Default case";
+        // break
+    }
+  }
+  return (
+    <div className="container">
+      <div className="box">
+        <h2>Password Validator</h2>
+
+        <div className="pass">
+          <input
+            type="text"
+            placeholder="Enter password"
+            className="pass-input"
+            value={pass}
+            onChange={(event)=>{
+              setp(event.target.value);
+              setm(getscore(event.target.value));
+            }}
+          ></input>
+        </div>
+        <br></br>
+        <br></br>
+
+        {msg && (
+          <div className="strength">
+            <small className={colour}>{msg} password</small>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+root.render(<Pass />);
